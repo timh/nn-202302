7591061a4398b93086181c7b4b8df2c5b0c3c6e7
Tim Hinderliter <tim@disorder.net>
2023-02-19 23:55:55 -0800

    new training loop. untested.

--- trainer.py
+++ trainer.py
@@ -1,158 +1,116 @@
+import random
+from typing import Tuple, Callable
+from dataclasses import dataclass
+from collections import defaultdict
 import datetime
+import string
 
-import torch
-from torch import nn
+import torch, torch.optim
 from torch.utils.data import DataLoader
-import numpy as np
+import torch.nn as nn
 
-import matplotlib.pyplot as plt
-from IPython import display
-import torchvision.utils as vutils
+# mine
+def DistanceLoss(out, truth):
+    return torch.abs((truth - out)).mean()
+
+# https://stats.stackexchange.com/questions/438728/mean-absolute-percentage-error-returning-nan-in-pytorch
+def MAPELoss(output, target):
+    return torch.mean(torch.abs((target - output) / (target + 1e-6)))
+
+def RPDLoss(output, target):
+  return torch.mean(torch.abs(target - output) / ((torch.abs(target) + torch.abs(output)) / 2))    
 
 class Trainer:
-    def __init__(self, dirname: str, 
-                 net: nn.Module, loss_fn: nn.Module, optimizer: torch.optim.Optimizer,
-                 grid_num_rows = 1, grid_num_cols = 1):
-        self.dirname = dirname
+    net: nn.Module
+    loss_fn: Callable[[torch.Tensor, torch.Tensor], torch.Tensor]
+    optim: torch.optim.Optimizer
+
+    # loss_fn(outputs, truth)
+    def __init__(self, net: nn.Module, loss_fn: Callable[[torch.Tensor, torch.Tensor], torch.Tensor], optim: torch.optim.Optimizer):
         self.net = net
         self.loss_fn = loss_fn
-        self.optimizer = optimizer
-        self.grid_num_rows = grid_num_rows
-        self.grid_num_cols = grid_num_cols
+        self.optim = optim
     
-    def train(self, dataloader: DataLoader, epochs: int, device: str = "", do_display = True):
-        self._num_batches = len(dataloader)
-        self._num_data = len(dataloader.dataset)
+    # override this for new behavior after each batch of training samples is
+    # processed. this is called after the torch.isnan() check.
+    def on_train_batch(self, inputs: torch.Tensor, truth: torch.Tensor, loss: torch.Tensor):
+        pass
 
-        fake_images = None
+    # override this for new behavior after each batch of validation samples is
+    # processed. this is called after the torch.isnan() check.
+    def on_val_batch(self, inputs: torch.Tensor, truth: torch.Tensor, loss: torch.Tensor):
+        pass
 
-        self._do_display = do_display
-        self._setup_fig()
+    # override this for new behavior after each epoch.
+    def on_epoch(self, epoch: int, train_loss_hist: torch.Tensor, val_loss_hist: torch.Tensor):
+        pass
 
-        self._loss_over_time = list()
+    def train(self, num_epochs: int, train_dataloader: DataLoader, val_dataloader: DataLoader):
+        train_loss_hist = torch.zeros((num_epochs,))
+        val_loss_hist = torch.zeros_like(train_loss_hist)
 
-        self._first_print_time = datetime.datetime.now()
-        self._last_print_time = self._first_print_time
-        self._last_print_step = 0
-        global_step = 0
+        last_print = datetime.datetime.now()
+        last_print_nsamples = 0
+        last_print_batch = 0
 
-        inputs, expected = next(iter(dataloader))
-        self._batch_size = len(inputs)
+        total_nsamples_sofar = 0
+        total_batch_sofar = 0
+        for epoch in range(num_epochs):
+            train_loss = 0.0
 
-        for epoch in range(epochs):
-            for batch, (inputs, expected) in enumerate(dataloader):
-                if device:
-                    inputs = inputs.to(device)
-                    expected = expected.to(device)
+            for batch, (inputs, truth) in enumerate(train_dataloader):
+                out = self.net(inputs)
+                loss = self.loss_fn(out, truth)
 
-                # gen outputs
-                if epoch == 0 and batch == 0:
-                    walk_modules(self.net, inputs)
-                fake_outputs = self.net(inputs)
+                if loss.isnan():
+                    # not sure if there's a way out of this...
+                    print(f"!! train loss {loss} at epoch {epoch}, batch {batch} -- returning!")
+                    return train_loss_hist[:epoch], val_loss_hist[:epoch]
+                train_loss += loss.item()
 
-                # back prop
-                loss = self.loss_fn(fake_outputs, expected)
-                self.net.zero_grad()
+                self.on_train_batch(epoch, inputs, truth, loss)
+
                 loss.backward()
-                loss = loss.item()
-                self._loss_over_time.append(loss)
-                self.optimizer.step()
+                self.optim.step()
 
-                self.maybe_print_status(epoch, epochs, global_step, batch, inputs, expected, fake_outputs, loss)
+                total_nsamples_sofar += len(inputs)
+                total_batch_sofar += 1
 
-                global_step += 1
+            train_loss /= len(train_dataloader)
 
-    def maybe_print_status(self, epoch: int, epochs: int, global_step: int, batch: int,
-                           inputs: torch.Tensor, expected: torch.Tensor,
-                           outputs: torch.Tensor,
-                           loss: float):
-        now = datetime.datetime.now()
-        delta_last = now - self._last_print_time
-        if delta_last >= datetime.timedelta(seconds=10) or (epoch == epochs and batch == self._num_batches):
-            delta_first = now - self._first_print_time
-            persec_first = global_step * self._batch_size / delta_first.total_seconds()
-            persec_last = (global_step - self._last_print_step) * self._batch_size / delta_last.total_seconds()
+            with torch.no_grad():
+                val_loss = 0.0
+                for batch, (inputs, truth) in enumerate(val_dataloader):
+                    val_out = self.net(inputs)
+                    loss = self.loss_fn(val_out, truth)
 
-            self.update_fig(epoch, expected, outputs)
+                    if loss.isnan():
+                        print(f"!! validation loss {loss} at epoch {epoch}, batch {batch} -- returning!")
+                        return train_loss_hist[:epoch], val_loss_hist[:epoch]
 
-            if self._do_display:
-                self.display_fig()
+                    val_loss += loss.item()
 
-            basename = f"{self.dirname}/epoch{epoch:05}-step{global_step:06}"
+                    self.on_val_batch(epoch, inputs, truth, loss)
 
-            print(f"epoch {epoch + 1}/{epochs}, batch {batch}/{self._num_batches} | loss {loss:.4f} | {persec_first:.4f} samples/sec")
+                val_loss /= len(val_dataloader)
 
-            img_filename = f"{basename}.png"
-            print(f"saving {img_filename}")
-            self._fig.savefig(img_filename)
+            now = datetime.datetime.now()
+            if (now - last_print) >= datetime.timedelta(seconds=5) or (epoch == num_epochs - 1):
+                timediff = (now - last_print)
 
-            net_filename = f"{basename}.torch"
-            print(f"saving {net_filename}")
-            torch.save(self.net, net_filename)
+                samples_diff = float(total_nsamples_sofar - last_print_nsamples)
+                samples_per_sec = samples_diff / timediff.total_seconds()
+                batch_diff = float(total_batch_sofar - last_print_batch)
+                batch_per_sec = batch_diff / timediff.total_seconds()
 
-            self._last_print_time = now
-            self._last_print_step = global_step
+                print(f"epoch {epoch+1}/{num_epochs}: train loss {train_loss:.5f}, val loss {val_loss:.5f} | samp/sec {samples_per_sec:.3f} | batch/sec {batch_per_sec:.3f}")
+                last_print = now
+                last_print_nsamples = total_nsamples_sofar
+                last_print_batch = total_batch_sofar
 
-    def _setup_fig(self):
-        fig = plt.figure(figsize=(15, 15))
-        axes_loss = fig.add_subplot(self.grid_num_rows, self.grid_num_cols, (self.grid_num_cols + 1, self.grid_num_cols + 2))
+            train_loss_hist[epoch] = train_loss
+            val_loss_hist[epoch] = val_loss
 
-        self._fig = fig
-        self._axes_loss = axes_loss
-
-    def update_fig(self, epoch: int, expected: torch.Tensor, outputs: torch.Tensor):
-        self._axes_loss.clear()
-        self._axes_loss.set_title("Loss")
-        self._axes_loss.plot(self._loss_over_time, label='gen')
-    
-    def display_fig(self):
-        display.clear_output(wait=True)
-        display.display(self._fig)
-
-class ImageTrainer(Trainer):
-    grid_num_images: int = 64
-
-    def __init__(self, dirname: str, net: nn.Module, loss_fn: nn.Module, optimizer: torch.optim.Optimizer, 
-                 grid_num_rows = 2, grid_num_cols = 2,
-                 grid_num_images = 64):
-        super().__init__(dirname, net, loss_fn, optimizer, grid_num_rows, grid_num_cols)
-        self.grid_num_images = grid_num_images
-
-    def update_fig(self, epoch: int, expected: torch.Tensor, outputs: torch.Tensor):
-        super().update_fig(epoch, expected, outputs)
-
-        # real_images = vutils.make_grid(expected[:self._grid_num_images], nrow=self._grid_rows, padding=2, normalize=True).cpu()
-        real_images = vutils.make_grid(expected[:self._grid_num_images], nrow=self._grid_rows, padding=2).cpu()
-
-        fake_images = outputs.reshape(expected.shape).detach().cpu()
-        # fake_images = vutils.make_grid(fake_images[:self._grid_num_images], nrow=self._grid_rows, padding=2, normalize=True).cpu()
-        fake_images = vutils.make_grid(fake_images[:self._grid_num_images], nrow=self._grid_rows, padding=2).cpu()
-
-        self._axes_real.clear()
-        self._axes_real.set_title("Real Images")
-        self._axes_real.imshow(np.transpose(real_images, (1, 2, 0)))
-
-        # Plot the fake images from the last epoch
-        self._axes_fake.clear()
-        self._axes_fake.set_title("Fake Images")
-        self._axes_fake.imshow(np.transpose(fake_images, (1, 2, 0)))
-
-    def _setup_fig(self):
-        super()._setup_fig()
-
-        axes_real = self._fig.add_subplot(self.grid_num_rows, self.grid_num_cols, 1)
-        axes_real.set_axis_off()
-        axes_fake = self._fig.add_subplot(self.grid_num_rows, self.grid_num_cols, 2)
-        axes_fake.set_axis_off()
-
-        self._axes_real = axes_real
-        self._axes_fake = axes_fake
-
-        self._grid_num_images = self.grid_num_images
-        self._grid_rows = int(np.sqrt(self.grid_num_images))
-
-
-def walk_modules(net: nn.Module, outputs: torch.Tensor):
-    for idx, (key, mod) in enumerate(net._modules.items()):
-        outputs = mod(outputs)
-        print(f"{idx}: {outputs.shape}")
+            self.on_epoch(epoch, train_loss_hist, val_loss_hist)
+        
+        return train_loss_hist, val_loss_hist
