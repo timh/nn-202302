fixed some notebook/GraphLogger off-by-one/other issues

notebook.py
@@ -38,8 +38,9 @@ class Plot:
     def add_data(self, idx: int, data: torch.Tensor):
         start = self._epochs_so_far[idx]
         end = start + len(data)
+        # print(f"  \033[1;33madd_data: {idx=} {start=} {end=}\033[0m")
         if end > len(self.data[idx]):
-            print(f"add_data: too much data: {start=} {end=} {self.data[idx].shape=}")
+            print(f"add_data: too much data: {idx=} {start=} {end=} {self.data[idx].shape=}")
         self.data[idx][start:end] = data
 
         self._epochs_so_far[idx] += len(data)
@@ -156,8 +157,6 @@ def imshow(net: nn.Module, fn: Callable[[torch.nn.Linear], torch.Tensor],
     running_col = 0
     for t in tensors:
         trows, tcols = t.shape
-        # print(f"trows {trows}, tcols {tcols}")
-        # print(f"[:{trows}, {running_col}:{running_col+tcols}] = {t.shape}")
         all_tensors[:trows, running_col:running_col+tcols] = t
         running_col += tcols
 

trading/main_int.py
@@ -89,9 +89,13 @@ lrs = [
     (5e-6, 2000),
     (1e-6, 2000),
 ]
+
+# TODO for debugging
+# lrs = [(lrpair[0], lrpair[1] // 50) for lrpair in lrs]
+
 exp_epochs = sum([lrpair[1] for lrpair in lrs])
 
-optim_fn = lambda exp, lr: torch.optim.AdamW(exp.net.parameters(), lr=lr)
+get_optimizer_fn = lambda exp, lr: torch.optim.AdamW(exp.net.parameters(), lr=lr)
 
 def experiments():
     for nquote in num_quotes:
@@ -108,10 +112,7 @@ def experiments():
                 yield exp
 
 num_experiments = len(num_quotes) * len(num_hidden) * len(hidden_size)
-tcfg = trainer.TrainerConfig(learning_rates=lrs, 
-                             get_optimizer_fn=optim_fn, 
-                             num_experiments=num_experiments, 
-                             experiments=experiments())
+tcfg = trainer.TrainerConfig(lrs, get_optimizer_fn, num_experiments, experiments())
 
 logger = PredLogger(exp_epochs, num_exps=num_experiments, fig_loss=fig_loss)
 tr = trainer.Trainer(logger)

trainer.py
@@ -91,14 +91,13 @@ class Trainer:
         return False
     
     def train(self, tcfg: TrainerConfig):
-        exp_epochs = tcfg.exp_epochs
-
         for i, exp in enumerate(tcfg.experiments):
-            exp.exp_epochs = exp_epochs
+            exp.exp_epochs = tcfg.exp_epochs
             exp.train_loss_hist = torch.zeros((exp.exp_epochs,))
             exp.val_loss_hist = torch.zeros_like(exp.train_loss_hist)
-            exp.exp_epochs = tcfg.exp_epochs
             exp.exp_idx = i
+            if self.logger is not None:
+                self.logger.on_exp_start(exp)
 
             exp_epoch = 0
             for lridx, (lr, lr_epochs) in enumerate(tcfg.learning_rates):
@@ -116,6 +115,9 @@ class Trainer:
 
                     self.on_epoch(exp, exp_epoch, lr_epoch)
                     exp_epoch += 1
+            
+            if self.logger is not None:
+                self.logger.on_exp_end(exp)
 
 class GraphLogger(TrainerLogger):
     fig_loss: Figure
@@ -144,10 +146,12 @@ class GraphLogger(TrainerLogger):
 
     def on_epoch(self, exp: Experiment, exp_epoch: int, lr_epoch: int):
         start = self.last_exp_epoch
-        end = exp_epoch
+        end = exp_epoch + 1
+        # print(f"\033[1;31mGraphLogger: {exp.exp_idx=}  |  {exp_epoch=} {lr_epoch=}  |  {start=} {end=}  |  {exp.exp_epochs=} {exp.lr_epochs=}\033[0m")
+        self.last_exp_epoch = exp_epoch + 1
 
-        self.plot_train.add_data(exp.exp_idx, exp.train_loss_hist[start:end + 1])
-        self.plot_val.add_data(exp.exp_idx, exp.val_loss_hist[start:end + 1])
+        self.plot_train.add_data(exp.exp_idx, exp.train_loss_hist[start:end])
+        self.plot_val.add_data(exp.exp_idx, exp.val_loss_hist[start:end])
 
         if exp.exp_idx == 0:
             learning_rates = torch.tensor([exp.cur_lr] * (end - start))
@@ -157,6 +161,6 @@ class GraphLogger(TrainerLogger):
         self.plot_train.render(0.8, 100)
         self.plot_val.render(0.8, 100)
 
-        annotate = lr_epoch == exp.lr_epochs
+        annotate = (lr_epoch + 1) == exp.lr_epochs
         self.plot_train.render(0.8, 100, annotate)
         self.plot_val.render(0.8, 100, annotate)