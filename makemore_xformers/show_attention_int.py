d1d9639e2cfa95f73bffdb52af26407737320050
Tim Hinderliter <tim@disorder.net>
2023-03-04 15:01:33 -0800

    more updates to show_attention_int

--- makemore_xformers/show_attention_int.py
+++ makemore_xformers/show_attention_int.py
@@ -17,8 +17,8 @@ import model
 import text_experiment
 import tokens
 
-model_filename = "runs/wt2raw_5000-seqlen 256, wordlen 1, nhead 6, nlayers 4, emblen 96, hidlen 384, optim adamw, sched nanogpt-cosine, startlr 1.00E-03, endlr 1.00E-04, batch 128, minicnt 4, epochs 5000, flash pytorch, compile True, elapsed 580.59s, vloss 0.000.ckpt"
-text_filename = "/home/tim/Downloads/wikitext-2-raw/wiki.train+valid.raw"
+model_filename = "runs/wt2raw_5000-seqlen 128, wordlen 1, nhead 6, nlayers 4, emblen 192, hidlen 768, optim adamw, sched nanogpt-cosine, startlr 1.00E-03, endlr 5.00E-04, batch 128, minicnt 4, epochs 5000, flash pytorch, compile True, elapsed 428.07s, vloss 1.380.ckpt"
+text_filename = "/home/tim/Downloads/wikitext-2-raw/wiki.valid.raw"
 
 with open(model_filename, "rb") as file:
     state_dict = torch.load(file)
@@ -30,15 +30,8 @@ with open(model_filename, "rb") as file:
 # %%
 
 treader = tokens.WordTextReader(exp.seqlen, exp.wordlen, text_filename, True, "cuda")
-dataloader = DataLoader(iter(treader), batch_size=1)
-input_toks, truth = next(iter(dataloader))
-
-_outputs, out_weights_all = exp.net(input_toks, return_weights=True)
-
-# for i, out_weights in enumerate(out_weights_all):
-#     print(f"{i}. {out_weights=}")
-
-out_weights_all = [w for out_weights in out_weights_all for w in out_weights[0]]
+dataloader = DataLoader(treader, batch_size=1, shuffle=True)
+dataloader_it = iter(dataloader)
 
 # %%
 
@@ -49,17 +42,30 @@ def format_fn(tick_val, tick_pos):
     
     return labels[tick_val]
 
+input_toks, truth = next(dataloader_it)
+_outputs, out_weights_all = exp.net(input_toks, return_weights=True)
+out_weights_all = [w for out_weights in out_weights_all for w in out_weights[0]]
+
+mean_weights = torch.zeros_like(out_weights_all[0])
+for w in out_weights_all:
+    mean_weights.add_(w)
+mean_weights = mean_weights / len(out_weights_all)
+out_weights_all.append(mean_weights)
+
+# just show the mean
+out_weights_all = out_weights_all[-1:]
+
 out_weights_all = [w.detach().cpu() for w in out_weights_all]
 # out_weights_all = out_weights_all[:5]
 labels = [treader.dictionary.token_to_vocab[tok.item()] for tok in input_toks[0]]
 
-max_side = 32
+max_side = 64
 labels = labels[:max_side]
-out_weights_plot = [out_weight[:max_side, :max_side] for out_weight in out_weights_all]
+out_weights_plot = [w[:max_side, :max_side] for w in out_weights_all]
 locater = MultipleLocator(base=1)
 
 nrows = len(out_weights_plot)
-fig_side = 10
+fig_side = 15
 fig = plt.figure(0, figsize=(fig_side, fig_side * nrows))
 axes = [fig.add_subplot(nrows, 1, i + 1) for i in range(len(out_weights_plot))]
 for i, w in enumerate(out_weights_plot):
