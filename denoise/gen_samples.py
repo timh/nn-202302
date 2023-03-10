bbaf2a5fa44f56b5d82b454c27d0213773420353
Tim Hinderliter <tim@disorder.net>
2023-03-10 13:56:24 -0800

    gen_samples: uses PIL.Image instead of matplotlib. added command line parsing. gussied it up a bit.

--- denoise/gen_samples.py
+++ denoise/gen_samples.py
@@ -1,18 +1,18 @@
 # %%
 import sys
 from pathlib import Path
-from typing import List, Union, Literal
+from typing import List, Union, Literal, Dict
 import re
 import csv
 import datetime
-import matplotlib.pyplot as plt
-from matplotlib.ticker import NullLocator
-from PIL import Image
-from IPython import display
+from PIL import Image, ImageDraw, ImageFont
+from fonts.ttf import Roboto
 import tqdm
+import argparse
 
 import torch
 from torch import Tensor
+from torchvision import transforms
 
 sys.path.append("..")
 import model
@@ -21,102 +21,156 @@ import denoise_logger
 
 device = "cuda"
 
+_img: Image.Image = None
+_draw: ImageDraw.ImageDraw = None
+_font: ImageFont.ImageFont = None
+_font_size = 10
+_padding = 2
+_minx = 50
+_miny = 150
+
+def _create_image(nrows: int, ncols: int, image_size: int,
+                  exps: List[Experiment], row_labels: List[str]):
+    global _img, _draw, _font
+    width = ncols * (image_size + _padding) + _minx
+    height = nrows * (image_size + _padding) + _miny
+    print(f"{width=} {height=}")
+    _img = Image.new("RGB", (width, height))
+    _draw = ImageDraw.ImageDraw(_img)
+    _font = ImageFont.truetype(Roboto, _font_size)
+
+def _build_ago_str(now: datetime.datetime, exp: Experiment):
+    ago = int((now - exp.curtime).total_seconds())
+    ago_secs = ago % 60
+    ago_mins = (ago // 60) % 60
+    ago_hours = (ago // 3600)
+    ago = [(val, desc) for val, desc in zip([ago_hours, ago_mins, ago_secs], ["h", "m", "s"])]
+    while not ago[0][0]:
+        ago.pop()
+    ago_str = " ".join([f"{val}{desc}" for val, desc in ago])
+    return ago_str
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--pattern", default=None)
+    parser.add_argument("-m", "--mode", type=str, choices=["latent", "random", "steps"])
+    parser.add_argument("-o", "--output", default=None)
+
+    cfg = parser.parse_args()
+
     checkpoints = denoise_logger.find_all_checkpoints()
-    # checkpoints = [cpres for cpres in checkpoints if "c32,c64,c64" in cpres.conv_descs and "_5000" in str(cpres.path)]
-    nrows = len(checkpoints)
+    if cfg.pattern:
+        checkpoints = [(path, exp) for path, exp in checkpoints if cfg.pattern in str(path)]
 
-    mode: Literal["latent-decoder", "random-start", "steps"] = "random-start"
+    checkpoints = sorted(checkpoints, key=lambda cptup: cptup[1].curtime)
+    if cfg.mode == "latent":
+        skipped_checkpoints = [(path, exp) for path, exp in checkpoints if exp.emblen == 0]
+        checkpoints = [(path, exp) for path, exp in checkpoints if exp.emblen != 0]
+        print(f"skipping because mode=latent and checkpoint emblen=0; they don't use any latent representation:")
+        [print(f"  {exp.label}") for path, exp in skipped_checkpoints]
+        print()
 
-    if mode == "steps":
-        steps_list = [2, 5, 10, 20, 30, 50, 70, 100]
-        col_labels = [f"{s} steps" for s in steps_list]
-        ncols = len(steps_list) + 1
-        filename = "gen-steps.png"
-    elif mode == "latent-decoder":
+    experiments = [exp for path, exp in checkpoints]
+    [print(exp.label) for exp in experiments]
+
+    image_size = experiments[0].image_size
+    nchannels = experiments[0].nchannels
+    image_size_list = [exp.image_size for exp in experiments]
+    nchannels_list = [exp.nchannels for exp in experiments]
+    if not all([onesz == image_size for onesz in image_size_list[1:]]):
+        raise Exception(f"not all dirs have image_size={image_size}: {image_size_list}")
+    if not all([onenc == nchannels for onenc in nchannels_list[1:]]):
+        raise Exception(f"not all dirs have nchannels={nchannels}: {nchannels_list}")
+    ncols = len(checkpoints)
+    padded_image_size = image_size + _padding
+
+    if cfg.mode == "steps":
+        steps_list = [2, 5, 10, 20, 40, 80]
+        row_labels = [f"{s} steps" for s in steps_list]
+        nrows = len(steps_list)
+        filename = cfg.output or "gen-steps.png"
+
+        # uniform distribution for pixel space.
+        inputs = torch.rand((1, nchannels, image_size, image_size), device=device)
+    elif cfg.mode == "latent":
         num_steps = 50
-        ncols = 10
-        col_labels = [f"latent {i}" for i in range(ncols)]
-        filename = "gen-latent-decoder.png"
-    else: # random-start
+        nrows = 10
+        row_labels = [f"latent {i}" for i in range(nrows)]
+        filename = cfg.output or "gen-latent.png"
+        inputs_for_emblen: Dict[int, Tensor] = dict()
+    else: # random
         num_steps = 50
-        ncols = 10
-        col_labels = [f"random input {i}" for i in range(ncols)]
-        filename = "gen-random-start.png"
+        nrows = 10
+        row_labels = [f"rand {i}" for i in range(nrows)]
+        filename = cfg.output or "gen-random.png"
+        # uniform distribution for pixel space.
+        inputs = torch.rand((ncols, 1, nchannels, image_size, image_size), device=device)
 
+    to_pil = transforms.ToPILImage("RGB")
 
-    base = 2.5
-    fig = plt.figure(1, figsize=(base * (ncols + 1), base * nrows))
-    # plt.axis('off')
-    axes_list = fig.subplots(nrows, ncols + 1)
+    print(f"   ncols: {ncols}")
+    print(f"   nrows: {nrows}")
+    print(f"    mode: {cfg.mode}")
+    print(f"filename: {filename}")
 
-    for axes, label in zip(axes_list[0][1:], col_labels):
-        axes.set_title(label)
+    _create_image(nrows=nrows, ncols=ncols, image_size=image_size, exps=experiments, row_labels=row_labels)
 
-    # checkpoints = sorted(checkpoints, key=lambda c: c.label)
-    checkpoints = sorted(checkpoints, key=lambda cp: cp.path.stat().st_ctime)
+    # draw row headers
+    for row, row_label in enumerate(row_labels):
+        xy = (0, _miny + row * padded_image_size)
+        _draw.text(xy=xy, text=row_label, font=_font, fill="white")
 
-    inputs: Tensor = None
-    for cidx, cp in tqdm.tqdm(list(enumerate(checkpoints))):
-        # print(f"{cidx + 1}/{len(checkpoints)}")
-        with open(cp.path, "rb") as checkpoint_file:
-            state_dict = torch.load(checkpoint_file)
+    # draw column headers
+    now = datetime.datetime.now()
+    for col, (path, exp) in enumerate(checkpoints):
+        # draw the title for this checkpoint
+        endlr_str = f" {exp.endlr}" if exp.endlr else ""
+        conv_descs_list = exp.conv_descs.split(",")
+        conv_descs_len = len(conv_descs_list)
+        conv_descs1 = ",".join(conv_descs_list[:conv_descs_len//3])
+        conv_descs2 = ",".join(conv_descs_list[conv_descs_len//3:conv_descs_len//3*2])
+        conv_descs3 = ",".join(conv_descs_list[conv_descs_len//3*2:])
+        conv_descs_str = f"{conv_descs1}\n  {conv_descs2}\n  {conv_descs3}"
+        ago_str = _build_ago_str(now, exp)
+        title = (f"{conv_descs_str}\n"
+                 f"emblen {exp.emblen}\n"
+                 f"nlinear {exp.nlinear} hidlen {exp.hidlen}\n"
+                 f"startlr {exp.startlr:.1E} {endlr_str}\n"
+                 f"nepoch {exp.nepochs}\n"
+                 f"tloss {exp.lastepoch_train_loss:.3f}\n"
+                 f"vloss {exp.lastepoch_val_loss:.3f}\n"
+                 f"{ago_str} ago")
 
-        tloss = state_dict["lastepoch_train_loss"]
-        vloss = state_dict["lastepoch_val_loss"]
-        started_at = state_dict["started_at"]
-        elr_str = f"elr {cp.elr:.1E}\n" if cp.elr else ""
-        title = (f"{cp.conv_descs}\n"
-                 f"emblen {cp.emblen}\n"
-                 f"nlin {cp.nlin}\n"
-                 f"hidlen {cp.hidlen}\n"
-                 f"slr {cp.slr:.1E}\n"
-                 f"{elr_str}"
-                 f"epoch {cp.epoch}\n"
-                 f"tloss {tloss:.3f}\n"
-                 f"vloss {vloss:.3f}\n"
-                 f"started_at {started_at}")
+        xy = (_minx + col * padded_image_size, 0)
+        _draw.text(xy=xy, text=title, font=_font, fill="white")
 
-        axes: plt.Axes = axes_list[cidx][0]
-        axes.text(0.5, 0.5, title, horizontalalignment='center', verticalalignment='center')
-        axes.axis('off')
+    for col, (path, exp) in tqdm.tqdm(list(enumerate(checkpoints))):
+        nchannels = exp.nchannels
 
-        exp = Experiment.new_from_state_dict(state_dict)
-        exp.net: model.ConvEncDec = model.ConvEncDec.new_from_state_dict(state_dict["net"]).to(device)
-        image_size = exp.net.image_size
-        nchannels = exp.net.nchannels
+        with open(path, "rb") as cp_file:
+            state_dict = torch.load(path)
+            exp.net = model.ConvEncDec.new_from_state_dict(state_dict["net"]).to(device)
 
-        if inputs is None:
-            if mode == "latent-decoder":
+        if cfg.mode == "latent":
+            if exp.emblen not in inputs_for_emblen:
                 # gaussian distribution for latent space.
-                inputs = torch.normal(0.0, 0.5, (ncols, 1, cp.emblen), device=device)
-            elif mode == "random-start":
-                # uniform distribution for pixel space.
-                inputs = torch.rand((ncols, 1, nchannels, image_size, image_size), device=device)
-            else:
-                # uniform distribution for pixel space.
-                inputs = torch.rand((1, nchannels, image_size, image_size), device=device)
+                inputs_for_emblen[exp.emblen] = torch.normal(0.0, 0.5, (nrows, 1, exp.emblen), device=device)
+            inputs = inputs_for_emblen[exp.emblen]
 
-        for col in range(ncols):
-            if mode == "interp-latent":
+        for row in range(nrows):
+            if cfg.mode == "latent":
                 steps = num_steps
-                out = exp.net.decoder(inputs[col])
-            elif mode == "random-start":
+                out = exp.net.decoder(inputs[row])
+            elif cfg.mode == "random":
                 steps = num_steps
-                out = model.generate(exp=exp, num_steps=steps, size=image_size, truth_is_noise=False, input=inputs[col], device=device)
+                out = model.generate(exp=exp, num_steps=steps, size=image_size, truth_is_noise=False, input=inputs[row], device=device)
             else:
-                steps = steps_list[col]
+                steps = steps_list[row]
                 out = model.generate(exp=exp, num_steps=steps, size=image_size, truth_is_noise=False, input=inputs, device=device)
 
-            out = out[0]
-            out = torch.permute(out, (1, 2, 0))   # (chan, width, height) -> (width, height, chan)
-            out = out.detach().cpu()
-
-            axes: plt.Axes = axes_list[cidx][col + 1]
-            axes.axis('off')
-            axes.imshow(out)
+            # draw this image
+            out = to_pil(out[0].detach().cpu())
+            xy = (_minx + col * padded_image_size, _miny + row * padded_image_size)
+            _img.paste(out, box=xy)
     
-    fig.tight_layout()
-    display.display(fig)
-    fig.savefig(filename)
-    fig.clear();
+    _img.save(filename)
