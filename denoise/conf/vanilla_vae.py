13ad24dde6f86f7c30b9112c1673d2bc22305b27
Tim Hinderliter <tim@disorder.net>
2023-03-14 16:59:07 -0700

    WIP config from pytorch-vae

--- denoise/conf/vanilla_vae.py
+++ denoise/conf/vanilla_vae.py
@@ -16,14 +16,18 @@ device: str
 exps: List[Experiment]
 
 hidden_dims_values = [
-    [32, 64, 128, 256, 512],
-    [16, 32, 64, 128, 256],
-    [32, 64, 128],
+    [32, 64, 128, 256, 512],  # from PyTorch-VAE
+    # [16, 32, 64, 128, 256],
+    # [32, 64, 128],
 ]
-kld_weight_values = [1.0]
+latent_dim_values = [128]      # from PyTorch-VAE
+kld_weight_values = [2.5e-4]   # from PyTorch-VAE
+#kld_weight_values += [2.5e-3, 2.5e-5]
 
 lr_values = [
-    (1e-3, 1e-4, "nanogpt"),
+    # (1e-3, 1e-4, "nanogpt"),
+    # (0.005, ) from 
+    (5e-3, 5e-4, "nanogpt") # 5e-3 = start for PyTorch-VAE
 ]
 loss_type_values = ["l1", "l2"]
 optim_type = "adamw"
@@ -40,30 +44,31 @@ def loss_fn(exp: Experiment, kld_weight: float, recons_loss_type: str) -> Callab
         net: model_vanvae.VanillaVAE = exp.net
         kld_loss = net.loss_function()
         recons_loss = recons_loss_fn(output, truth)
-        return recons_loss + kld_weight + kld_loss
+        loss = recons_loss + kld_weight * kld_loss
+        # print(f"loss: kld={kld_loss:.5f} {recons_loss_type}={recons_loss:.5f}: result={loss:.5f}")
+        return loss
     return fn
 
 for hidden_dims in hidden_dims_values:
-    # latent_dim = hidden_dims[-1] * (len(hidden_dims) ** 2)
-    latent_dim = hidden_dims[-1]
-    for kld_weight in kld_weight_values:
-        for loss_type in loss_type_values:
-            for startlr, endlr, sched_type in lr_values:
-                hdims_str = "_".join(map(str, [h for h in hidden_dims]))
-                label = f"lat_dim_{latent_dim}"
-                label += f",hid_dims_{hdims_str}"
-                label += f",kld_weight_{kld_weight:.2f}"
-                label += f",loss_{loss_type}+kld"
-                
-                net_args = dict(
-                    in_channels=3, hidden_dims=hidden_dims, latent_dim=latent_dim, image_size=cfg.image_size,
-                )
-                exp_args = net_args.copy()
-                exp = Experiment(label=label, lazy_net_fn=lazy_net_fn(net_args),
-                                    startlr=startlr, endlr=endlr, 
-                                    optim_type=optim_type, sched_type=sched_type)
-                exp.loss_fn = loss_fn(exp, kld_weight, loss_type)
-                exps.append(exp)
+    for latent_dim in latent_dim_values:
+        for kld_weight in kld_weight_values:
+            for loss_type in loss_type_values:
+                for startlr, endlr, sched_type in lr_values:
+                    hdims_str = "_".join(map(str, [h for h in hidden_dims]))
+                    label = f"lat_dim_{latent_dim}"
+                    label += f",hid_dims_{hdims_str}"
+                    label += f",kld_weight_{kld_weight:.1E}"
+                    label += f",loss_{loss_type}+kld"
+                    
+                    net_args = dict(
+                        in_channels=3, hidden_dims=hidden_dims, latent_dim=latent_dim, image_size=cfg.image_size,
+                    )
+                    exp_args = net_args.copy()
+                    exp = Experiment(label=label, lazy_net_fn=lazy_net_fn(net_args),
+                                        startlr=startlr, endlr=endlr, 
+                                        optim_type=optim_type, sched_type=sched_type)
+                    exp.loss_fn = loss_fn(exp, kld_weight, loss_type)
+                    exps.append(exp)
 
 print(f"{len(exps)=}")
 # import random
